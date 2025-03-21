d451e960c94e33edca1cd13a2b06278ad6166d50
seahpark247 <famousjedo@gmail.com>
2025-03-20 22:12:29 -0500

    Initial Commit

---
+++ IKnowTheColor/IKnowTheColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IKnowTheColorApp.swift
+//  IKnowTheColor
+//
+//  Created by Seah Park on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct IKnowTheColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IKnowTheColorTests/IKnowTheColorTests.swift
@@ -0,0 +1,17 @@
+//
+//  IKnowTheColorTests.swift
+//  IKnowTheColorTests
+//
+//  Created by Seah Park on 3/20/25.
+//
+
+import Testing
+@testable import IKnowTheColor
+
+struct IKnowTheColorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
